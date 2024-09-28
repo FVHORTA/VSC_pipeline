a47dfa7aefaf49c05fe67dc451fa6a8237151ae5
FVHORTA <fvhorta@icloud.com>
2024-09-27 23:38:07 -0300

    numero2

--- teste.py
+++ teste.py
@@ -4,7 +4,7 @@ from datetime import datetime, time
 def main():
     st.title('Title')
     nome = st.text_input('Nome', max_chars=120).upper()
-    idade = st.number_input('Idade', min_value=0, max_value=120, step=1, value=0, format='%d')
+    idade = st.number_input('Idade', min_value=0, max_value=120, step=1, value=13, format='%d')
     data = st.date_input('Data', value=datetime.now(), min_value=datetime(1920, 12, 1))
     hora_atual = st.time_input ('Hora')
     email = st.text_input('E-mail')
