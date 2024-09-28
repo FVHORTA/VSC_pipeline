451f89bbc52594c9ec65d84d4e10db9adb9452b8
FVHORTA <fvhorta@icloud.com>
2024-09-27 23:21:04 -0300

    mumero

--- teste.py
+++ teste.py
@@ -4,10 +4,8 @@ from datetime import datetime, time
 def main():
     st.title('Title')
     nome = st.text_input('Nome')
-    idade = st.number_input('Idade',max_value=120)
-    data = st.date_input('Data', value=datetime.now(), min_value=datetime(1920, 12, 1)).strftime('%d/%m/%Y')
-    dataf = data.strftime('%d/%m/%Y')
-    status = st.selectbox('Status', ['S', 'N' ])
+    idade = st.number_input('Idade', min_value=0, max_value=120, step=1, value=0, format='%d')
+    data = st.date_input('Data', value=datetime.now(), min_value=datetime(1920, 12, 1))
     hora_atual = st.time_input ('Hora')
     email = st.text_input('E-mail')
     st.write(f'Hora atual: {hora_atual}')
@@ -31,4 +29,3 @@ if __name__ == '__main__':
 
     main()
 
-#principal
